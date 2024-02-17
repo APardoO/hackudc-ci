bef81a55248899a52a698678fadb76d025f8cd25
Talleyrand-34 <talleyrand-34@tutanota.com>
2024-02-17 16:28:00 +0200

    launch codee commands

---
+++ scripts/codee-launch.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+./codee/bin/pwreport mbedtls/library/*.c  --json > codee-report.json
+./codee/bin/pwreport --checks --verbose --config mbedtls/build/compile_commands.json --json > ./proc-json/codee-checks.json
+./codee/bin/pwreport mbedtls/build/* --screening --config mbedtls/build/compile_commands.json --json > codee-screening.json
+./codee/bin/pwreport --checks --verbose --config mbedtls/build/compile_commands.json --sarif > codee-check.sarif
