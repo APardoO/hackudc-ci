7ce35d7f41370886090b48de1463443c245e1e67
mrx <mrx@localhost.localdomain>
2024-02-17 19:03:23 +0100

    Modificacion por comprobación de variables de entorno

--- scripts/codee-launch.sh
+++ scripts/codee-launch.sh
@@ -6,7 +6,8 @@
 pwreport mbedtls/library/*.c --config mbedtls/build/compile_commands.json --json > codee-report.json
 pwreport --checks --verbose --config mbedtls/build/compile_commands.json --json > proc-json/codee-checks.json
 
-cmake -DCMAKE_C_COMPILER=gcc -DCMAKE_EXPORT_COMPILE_COMMANDS=On -DCMAKE_BUILD_TYPE=Release -B build -G "Unix Makefiles" mbedtls
+cd mbedtls
+cmake -DCMAKE_C_COMPILER=gcc -DCMAKE_EXPORT_COMPILE_COMMANDS=On -DCMAKE_BUILD_TYPE=Release -B build -G "Unix Makefiles" .
 cmake -C build
 
 # ../codee/bin/pwreport ../mbedtls/build/* --screening --config ../mbedtls/build/compile_commands.json --json > ../proc-json/codee-screening.json
