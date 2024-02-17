3f3bc1f11263a1b6d1d1047ce0ec0b9753420277
Talleyrand-34 <talleyrand-34@tutanota.com>
2024-02-17 16:52:29 +0200

    sarfi file transform to html

--- scripts/codee-launch.sh
+++ scripts/codee-launch.sh
@@ -4,3 +4,4 @@
 ../codee/bin/pwreport --checks --verbose --config ../mbedtls/build/compile_commands.json --json > ../proc-json/codee-checks.json
 ../codee/bin/pwreport ../mbedtls/build/* --screening --config ../mbedtls/build/compile_commands.json --json > ../proc-json/codee-screening.json
 ../codee/bin/pwreport --checks --verbose --config ../mbedtls/build/compile_commands.json --sarif > ../codee-check.sarif
+sarif html ../codee-check.sarif -o ../html/
