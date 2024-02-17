4f83c69344cd83091a52acba7e625dc5856c54aa
Talleyrand-34 <talleyrand-34@tutanota.com>
2024-02-17 15:28:28 +0000

    solve initilitation external repos

--- scripts/setup-mbtls.sh
+++ scripts/setup-mbtls.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 #
-git clone https://github.com/Mbed-TLS/mbedtls ..
+git clone https://github.com/Mbed-TLS/mbedtls ../mbedtls
+echo "Run following commands on mbdetls on git root"
+echo "cmake -DCMAKE_C_COMPILER=gcc -DCMAKE_EXPORT_COMPILE_COMMANDS=On -DCMAKE_BUILD_TYPE=Release -B build -G "Unix Makefiles" ."
+echo "make -C build"
