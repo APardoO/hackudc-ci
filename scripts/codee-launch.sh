88cb6612649c9ad341547bd4ac696edf30c370a0
mrx <mrx@localhost.localdomain>
2024-02-17 19:28:27 +0100

    Modificacion por comprobación de variables de entorno

--- scripts/codee-launch.sh
+++ scripts/codee-launch.sh
@@ -3,12 +3,12 @@
 #codee/bin/pwreport ../mbedtls/library/*.c --config ../mbedtls/build/compile_commands.json --json > ../codee-report.json
 #codee/bin/pwreport --checks --verbose --config ../mbedtls/build/compile_commands.json --json > ../proc-json/codee-checks.json
 
-pwreport mbedtls/library/*.c --config mbedtls/build/compile_commands.json --json > codee-report.json
-pwreport --checks --verbose --config mbedtls/build/compile_commands.json --json > proc-json/codee-checks.json
-
 cd mbedtls
 cmake -DCMAKE_C_COMPILER=gcc -DCMAKE_EXPORT_COMPILE_COMMANDS=On -DCMAKE_BUILD_TYPE=Release -B build -G "Unix Makefiles" .
 cmake -C build
+
+CODEE_ACCEPT_EULA=yes && pwreport mbedtls/library/*.c --config mbedtls/build/compile_commands.json --json > codee-report.json
+CODEE_ACCEPT_EULA=yes && pwreport --checks --verbose --config mbedtls/build/compile_commands.json --json > proc-json/codee-checks.json
 
 # ../codee/bin/pwreport ../mbedtls/build/* --screening --config ../mbedtls/build/compile_commands.json --json > ../proc-json/codee-screening.json
 # ../codee/bin/pwreport --checks --verbose --config ../mbedtls/build/compile_commands.json --sarif > ../codee-check.sarif
