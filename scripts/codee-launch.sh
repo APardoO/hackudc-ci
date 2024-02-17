d2eecd60b11e0810f23a7f5cdee5063d7aeace46
mrx <mrx@localhost.localdomain>
2024-02-17 22:59:48 +0100

    bugfixes commented commands

--- scripts/codee-launch.sh
+++ scripts/codee-launch.sh
@@ -11,6 +11,9 @@
 CODEE_ACCEPT_EULA=yes && pwreport mbedtls/library/*.c --config mbedtls/build/compile_commands.json --json > codee-report.json
 CODEE_ACCEPT_EULA=yes && pwreport --checks --verbose --config mbedtls/build/compile_commands.json --json > proc-json/codee-checks.json
 
-# ../codee/bin/pwreport ../mbedtls/build/* --screening --config ../mbedtls/build/compile_commands.json --json > ../proc-json/codee-screening.json
-# ../codee/bin/pwreport --checks --verbose --config ../mbedtls/build/compile_commands.json --sarif > ../codee-check.sarif
-# sarif html ../codee-check.sarif -o ../html/
+CODEE_ACCEPT_EULA=yes && codee/bin/pwreport ../mbedtls/build/* --screening --config ../mbedtls/build/compile_commands.json --json > ../proc-json/codee-screening.json
+CODEE_ACCEPT_EULA=yes && codee/bin/pwreport --checks --verbose --config ../mbedtls/build/compile_commands.json --sarif > ../codee-check.sarif
+sarif html ../codee-check.sarif -o ../html/
+python3 ../proc-json/order.py
+python3 ../proc-json/autofix.py
+
